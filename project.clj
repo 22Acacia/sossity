75b8b1d6243a96a703480214e9c1c1d70a6934fb
lusciouspear <taylor69>
2015-10-27 15:17:06 +0000

    added graph creator

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [aysylu/loom "0.5.4"]
-                 ])
+                 [aysylu/loom "0.5.4"]]
+  :plugins [[lein-cljfmt "0.3.0"]])
