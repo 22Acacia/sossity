e61439c4dcdbe2571228775549a5d6e53d837a20
Viktor Trako <viktor.trako@gmail.com>
2017-01-31 17:24:55 +0000

    DP-731 change the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sossity "0.1.1-SNAPSHOT"
+(defproject sossity "0.2.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
